3e149f5cfecee47faebb7de34575b11c3c5e2bbe
nabilChemkhi <nabil.chemkhi@esprit.tn>
2024-02-13 20:16:32 +0100

    i18n deploy pb fixed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -99,7 +99,7 @@ i18next
     lng: 'en',
     fallbackLng: 'en',
     backend: {
-      loadPath: 'assets/i18n/{{lng}}.json',
+      loadPath: '/portfolio/assets/i18n/{{lng}}.json',
     },
   });
 
